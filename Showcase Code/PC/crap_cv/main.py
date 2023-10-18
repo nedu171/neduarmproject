50cda5214ebe0beaff4b1b899ceedeccbc23f3bb
Adelene <47531026+AdeleneLSW15@users.noreply.github.com>
2023-10-18 16:48:11 +0100

    added comments to main cv

--- Showcase Code/PC/crap_cv/main.py
+++ Showcase Code/PC/crap_cv/main.py
@@ -129,7 +129,8 @@ def list_ports():
 
 class Main:
     PIECES_WAIT = 15
-    ALLOCATIONS = {
+    ALLOCATIONS = { 
+        #allocate rows as 1st matrix, column as 2nd matrix
         (0, 0): (ROOK, WHITE),
         (1, 0): (KNIGHT, WHITE),
         (2, 0): (BISHOP, WHITE),
@@ -163,13 +164,14 @@ class Main:
         (6, 7): (KNIGHT, BLACK),
         (7, 7): (ROOK, BLACK),
     }
-
+    #use default camera.
     def __init__(self, camera=0):
         self.images = {}
-
+        #aruco is the QR code library. Gets aruco markers.
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
         self.aruco_params = aruco.DetectorParameters_create()
-
+        #start video capture.
+        #set video height and width, cropping individual sqaures
         self.vid = cv2.VideoCapture(camera, cv2.CAP_DSHOW)
         self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, VIDEO_WIDTH)
         self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, VIDEO_HEIGHT)
@@ -179,21 +181,25 @@ class Main:
         self.pieces = {}
         self.piece_allocations = {}
 
+        #width of the square in chess square? Fed it into the position matrix
         self.square_w = 100
 
         self.grey = None
         self.points = None
 
         # Really don't care to be honest
+        #get rid of error messages
         import logging
         log = logging.getLogger('werkzeug')
         log.setLevel(logging.ERROR)
-
+        #creates web app using Flask
         self.app = Flask(__name__)
         self.app.route("/")(self.flask_route)
-
+        
+        #running - true (intializer)
         self.running = True
 
+    #tk is for the graphical user interface (ignore this for now)
     def _make_tk(self):
         self.tk = Tk()
         self.tk.title("CRAP Eyesight")
@@ -268,22 +274,24 @@ class Main:
 
         self.tk.update()
 
+    #associate each piece to a position on the board. self.pieces[i] etc.
+    #extract these parts. [i,j] where i is column and j is arrays.
     def bake_allocations(self):
         self.piece_allocations.clear()
         for i in self.pieces:
             allocation = self.ALLOCATIONS.get((self.pieces[i][0], self.pieces[i][1]))
             if allocation is not None:
                 self.piece_allocations[i] = allocation
-
+    #save position
     def save(self):
         with open(POINTS_CACHE, "wb") as f:
-            pickle.dump(self.points, f)
-
+            pickle.dump(self.points, f) #save into file
+    #load position
     def load(self):
         if os.path.exists(POINTS_CACHE):
             with open(POINTS_CACHE, "rb") as f:
-                self.points = pickle.load(f)
-
+                self.points = pickle.load(f) #opens for reading in binary mode.
+    #remove this part - aruco related.
     def identify_aruco(self, img, do_mask=True):
         if do_mask:
             grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -298,20 +306,24 @@ class Main:
         )
         return corners, ids
 
+    
     def get_frame(self):
         ret, img = self.vid.read()
         if not ret:
             print("TITSUP")
             quit()
 
+        #gaussian blur - identify it even if the image is blur.
+        #enhance image data via Gaussian bur and add Weight
         if self.sharpen.get():
             blurred = cv2.GaussianBlur(img, (0, 0), 3)
             return cv2.addWeighted(img, 1.5, blurred, -0.5, 0, blurred)
 
         return img
-
+    #creates binary mark. Grayscale
     def get_mask(self, img):
         self.grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #automatically determine the threshold of the mask. In order to detect black and white.
         _, msk = cv2.threshold(
             self.grey, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU
         )
@@ -320,16 +332,22 @@ class Main:
             self.show(WINDOW_MASK, msk)
 
         return msk
-
+    #create rectangular region, transform the image into wrapped image (undistorted,, front-facing concept)
     def extract(self, img, points, w=50, h=50, p=0):
         out_pts = np.float32(
             [(p, p), (w - 1 - p, p), (p, h - 1 - p), (w - 1 - p, h - 1 - p)]
         )
+        #getPerspective..(src,dst,inputArray,size, int flags)
         matrix = cv2.getPerspectiveTransform(points, out_pts)
+        #return the undistored image using bilinear interpolation - resampling method that uses distance weight average
+        #of 4 nearest pixels to estimate new pixel values (new squares).
         return cv2.warpPerspective(img, matrix, (w, h), flags=cv2.INTER_LINEAR)
-
+    
+    #relates to the chess square dimensions. Cut up board intos sqaures and stich them up together after that.
+    #crop out 8x8
     def get_8x8_regions(self, img, points):
         regions = [[None for _ in range(8)] for _ in range(8)]
+        #2 loop times for rows and columns. X for rows and Y for columns.
         for x in range(8):
             for y in range(8):
                 regions[x][y] = self.extract(
@@ -346,7 +364,7 @@ class Main:
                     self.square_w,
                 )
         return regions
-
+    #stich 8x8 squares
     def stitch_8x8(self, regions):
         full_w, full_h = self.square_w * 8 + 90, self.square_w * 8 + 90
         out = np.zeros((full_w, full_h, 3), dtype=np.uint8)
@@ -358,12 +376,12 @@ class Main:
                     x * (self.square_w + 10) + 10: (x + 1) * (self.square_w + 10),
                 ] = regions[x][y]
         return out
-
+    # uses QR codes to identify each square (commented out)
     def process_regions(self, regions):
         # for x in range(8):
         #     for y in range(8):
         #         self.identify_aruco(regions[x][y])
-
+        #if show is true -> stich up the 8x8 sqaures.
         if SHOW_REGIONS:
             stitched = self.stitch_8x8(regions)
             self.show(WINDOW_SQUARES, stitched)
@@ -399,12 +417,13 @@ class Main:
         objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2) * 30
 
         if ret:
+            #update self.points if found the corners. self.points contain positions
             self.points = extrapolate_points((7, 7), corners)
-
+            #optional - show chessboard corners and smooths it on the window (the grid on frame)
             if SHOW_CHESS_MARKERS:
                 fnl = cv2.drawChessboardCorners(img.copy(), (7, 7), corners, ret)
                 self.show(WINDOW_MARKERS, fnl)
-
+    #calibration process using aruco marker (ignore this part)
     def update_cal_quick(self, img):
         corners, ids = self.identify_aruco(img, False)
         if corners is None or ids is None:
@@ -447,11 +466,13 @@ class Main:
 
         if SHOW_QUICK_CAL:
             self.show(WINDOW_QUICK_CAL, im)
-
+    #display focus region within input image.
     def show_focus_region(self, img):
+        #check self points attributes
         if self.points:
-
-            pad = 10
+            
+            #why is pad 10?? Padding makes size larger
+            pad = 10 
             focus = self.extract(
                 img,
                 np.float32(
@@ -462,30 +483,39 @@ class Main:
                         self.points[8][8],
                     ]
                 ),
+                #takes length of square times by 8. (full square in the chessboard parameter )
                 self.square_w * 8,
                 self.square_w * 8,
                 pad,
             )
             if SHOW_EXTRAPOLATED:
+                #visualize calibration point from chessboard using self.points method
                 cb = draw_points(img, self.points)
                 self.show(WINDOW_EXTRAP_BOARD, cb)
+            #displays it with aruco chess piece (identify aruco markers in focus region)
             corners, ids = self.identify_aruco(focus, True)
             if SHOW_BOARD:
+                #draws the detected aruco on focused region. Monitor positions in the board.
                 aruco.drawDetectedMarkers(focus, corners, ids)
 
             for i in {**self.pieces}:
+                #decrements and pops the element out (if counter == 0). Might be related to aruco markers?
                 self.pieces[i][2] -= 1
                 if self.pieces[i][2] == 0:
                     self.pieces.pop(i)
 
             if corners is not None and ids is not None:
+                #binds aruco and ids together. Padding - simplify detection.
+                #for loop gets the position of aruco markers and store in self.pieces
                 for points, id_ in zip(corners, ids):
                     centre = (np.sum(points, axis=1) / 4)[0]
                     x = int((centre[0] - pad) // self.square_w)
                     y = int((centre[1] - pad) // self.square_w)
-                    self.pieces[id_[0]] = [x, y, self.PIECES_WAIT]
+                    self.pieces[id_[0]] = [x, y, self.PIECES_WAIT] #wait is a counter - defined in the start of the code (15)
 
                     if SHOW_BOARD:
+                        #draws circle around aruco marker. displays the x,y coordinates of aruco markets.
+                        #operate within the focus region.
                         cv2.circle(focus, (int(centre[0]), int(centre[1])), 5, (0, 255, 0))
                         cv2.putText(
                             focus,
@@ -502,38 +532,47 @@ class Main:
     def serialize(self):
         out = ""
         for y in range(8):
-            line = []
+            #start loop for the x, y coordinates
+            line = [] #stores empty position in an array.
 
             for x in range(8):
                 for piece in self.pieces:
+                    #take piece information
                     x1, y1, _ = self.pieces[piece]
                     if x1 == x and y1 == y:
+                        #Match piece to position on a board. 
                         allocation = self.piece_allocations.get(piece)
                         if allocation is None:
+                            #not found
                             line.append("??")
                         else:
+                            #giving it color (Black or white) + type of pieces.
                             line.append(("B" if allocation[1] else "W") + "RBNKQP"[allocation[0]])
-                        break
+                        break #break loop, does per square per chess piece type
                 else:
-                    line.append("--")
-            out += " ".join(line) + "\n"
+                    line.append("--") #if no piece found in square, return -- (no piece on that square)
+            out += " ".join(line) + "\n" #adds all info in line array into serialized string with each piece information
         return out
-
+    #GUI
     def flask_route(self):
         return self.serialize()
-
+    #process input image, anotates chess pieces, updates the regions.
     def detect(self, img):
         if self.live_cal.get():
             if self.quick_cal.get():
+                #if quick is true - execute update via quick call
                 self.update_cal_quick(img)
             else:
+                #if not - normal cal update
                 self.update_cal(img)
 
         if self.points:
+            #extract 8x8 (chess squares)
             regions = self.get_8x8_regions(img, self.points)
-
+            #t
             for piece in self.pieces:
                 (x, y, _) = self.pieces[piece]
+                #annotates 8x8 region by coordinates. show coordinates on image via text.
                 cv2.putText(
                     regions[x][y],
                     str(piece),
@@ -543,6 +582,7 @@ class Main:
                     (255, 0, 0),
                     2,
                 )
+                #allocate individual pieces (like searialize function), show them via text.
                 allocation = self.piece_allocations.get(piece)
                 if allocation is not None:
                     cv2.putText(
@@ -563,25 +603,28 @@ class Main:
                         (0, 255, 0),
                         2,
                     )
-
+            #show focus region and process regions
             self.show_focus_region(img)
             self.process_regions(regions)
 
     def mainloop(self):
+        #run app via thread (concurrent?) for Flask web-app
         threading.Thread(target=self.app.run, kwargs={"port":3000}, daemon=True).start()
-
+        #Allow user to quit the program by turning running true to false.
         signal.signal(signal.SIGINT, lambda *_: setattr(self, "running", False))
 
+        #program runs
         while self.running:
             img = self.get_frame()
             self.detect(img)
-            cv2.waitKey(1)
-            self.render_tk()
-
+            cv2.waitKey(1) #allow user input (1ms)
+            self.render_tk() #render the GUI
+        #if programs stops running - 'goodbye'
         print("Goodbye!")
-
+        #destroy gui when program stops
         if self.tk is not None:
             self.tk.destroy()
+        #release video
         self.vid.release()
 
 
